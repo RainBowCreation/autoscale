30acd20fc29ac33b2509d40f2103351b8a678d81
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-07 18:27:29 +0700

    Create install.sh

---
+++ lib/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.deb
+sudo yum install ~/Downloads/jdk-20_linux-x64_bin.deb
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-20/bin/java 1
+sudo update-alternatives --install /usr/bin/java javac /usr/lib/jvm/jdk-20/bin/javac 1
+sudo update-alternatives --install /usr/bin/java jar /usr/lib/jvm/jdk-20/bin/jar 1
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+sudo update-alternatives --config jar
