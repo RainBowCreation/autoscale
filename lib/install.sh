872cade8d9bf4d3b95100937d17b46417ab2f9e6
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-07 19:00:13 +0700

    Update install.sh

--- lib/install.sh
+++ lib/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.deb
-cd ..
-sudo yum install lib/jdk-20_linux-x64_bin.deb
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-20/bin/java 1
-sudo update-alternatives --install /usr/bin/java javac /usr/lib/jvm/jdk-20/bin/javac 1
-sudo update-alternatives --install /usr/bin/java jar /usr/lib/jvm/jdk-20/bin/jar 1
-sudo update-alternatives --config java
-sudo update-alternatives --config javac
-sudo update-alternatives --config jar
+ sudo rpm --import https://yum.corretto.aws/corretto.key 
+ sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+ sudo yum install -y java-20-amazon-corretto-devel
+ sudo alternatives --config java 1
