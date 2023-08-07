bc019ed0d89865620b6e06d426797800607787cc
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-08 03:31:36 +0700

    Update start.sh

--- node/start.sh
+++ node/start.sh
@@ -4,4 +4,4 @@
 MULTIPAPER_NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
 
 # start multipaper node
-java -Dmultipaper.master-connection.master-address=vocanicz.thddns.net:2997 -Dmultipaper.master-connection.my-name=$MULTIPAPER_NODE_NAME -Xmx4G -Xms64m -jar multipaper-1.19.2-37.jar --nogui #-Dspigot.settings.bungeecord=true -DbungeecordName=$MULTIPAPER_NODE_NAME -DmultipaperMasterAddress=vocanicz.thddns.net:2997
+java -Dmultipaper.master-connection.master-address=vocanicz.thddns.net:2997 -Dmultipaper.master-connection.my-name=$MULTIPAPER_NODE_NAME -Xmx4G -Xms64m -jar multipaper-1.19.2-37.jar --nogui
