1d7e161fed50319bc99be3ac5222cbcc86e1898a
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-09 02:37:04 +0700

    Update start.sh

--- node/start.sh
+++ node/start.sh
@@ -4,7 +4,7 @@
 rm -rf plugins
 
 # generate random id
-MULTIPAPER_NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
+NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
 
 # start multipaper node
-java -Dmultipaper.master-connection.master-address=vocanicz.thddns.net:2997 -Dmultipaper.master-connection.my-name=$MULTIPAPER_NODE_NAME ${JAVA_OPTS} -jar multipaper-1.19.2-37.jar --nogui
+java ${JAVA_OPTS} -jar Magma-1.12.2-47c7ac0b.jar --nogui
