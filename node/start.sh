2f4f7f5750da553e63d2820d0f1babc2bf9d708e
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-08 13:07:47 +0700

    Update start.sh

--- node/start.sh
+++ node/start.sh
@@ -4,4 +4,4 @@
 MULTIPAPER_NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
 
 # start multipaper node
-java -Dmultipaper.master-connection.master-address=vocanicz.thddns.net:2997 -Dmultipaper.master-connection.my-name=$MULTIPAPER_NODE_NAME -Xmx4G -Xms64m -jar multipaper-1.19.2-37.jar --nogui
+java -Dmultipaper.master-connection.master-address=vocanicz.thddns.net:2997 -Dmultipaper.master-connection.my-name=$MULTIPAPER_NODE_NAME ${JAVA_OPTS} -jar multipaper-1.19.2-37.jar --nogui
