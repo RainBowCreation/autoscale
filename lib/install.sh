764f6749498e303f350f67b07ed4514b5a923a64
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-09 13:13:45 +0700

    Update install.sh

--- lib/install.sh
+++ lib/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  sudo rpm --import https://yum.corretto.aws/corretto.key 
  sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
- sudo yum install -y java-1.8.0-amazon-corretto-devel
+ sudo yum install -y java-20-amazon-corretto-devel
  sudo alternatives --config java 1
