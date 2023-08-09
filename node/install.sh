a0f16ca69e21f17c316187bb6988dc8d7a108ab9
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-09 13:12:20 +0700

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://git.magmafoundation.org/api/v4/projects/7/packages/maven/org/magmafoundation/Magma/1.12.2-47c7ac0b/Magma-1.12.2-47c7ac0b.jar
+wget https://git.magmafoundation.org/api/v4/projects/103/packages/maven/org/magmafoundation/Magma/1.19.3-44.1.23-6e6ce905/Magma-1.19.3-44.1.23-6e6ce905-server.jar
