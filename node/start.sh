c8f49d77c38b2f2c595c69947b382a12a593b7a5
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-09 13:12:36 +0700

    Update start.sh

--- node/start.sh
+++ node/start.sh
@@ -7,4 +7,4 @@ rm -rf plugins
 NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
 
 # start multipaper node
-java ${JAVA_OPTS} -jar Magma-1.12.2-47c7ac0b.jar --nogui
+java ${JAVA_OPTS} -jar Magma-1.19.3-44.1.23-6e6ce905-server.jar --nogui
