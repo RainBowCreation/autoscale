945d100ccebab28e9db19a7877d2749e04228ee1
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-08-17 20:40:37 +0700

    -y java-debian

--- lib/install-debian.sh
+++ lib/install-debian.sh
@@ -5,4 +5,4 @@ sudo apt install apt-transport-https lsb-release ca-certificates curl dirmngr gn
 sudo sh -c 'echo "deb http://deb.debian.org/debian $(lsb_release -sc) main contrib non-free" >> /etc/apt/sources.list.d/debian.list'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 sudo apt update
-sudo apt install openjdk-17-jdk
+sudo apt install openjdk-17-jdk -y
