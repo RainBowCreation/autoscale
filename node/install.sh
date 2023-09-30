901adbe4f2b1a01235eb39030e53bf3444fb6e7d
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-09-30 13:04:03 +0700

    Add essentialx

--- node/install.sh
+++ node/install.sh
@@ -6,6 +6,8 @@ wget https://github.com/ViaVersion/ViaBackwards/releases/download/4.8.0/ViaBackw
 wget https://download.luckperms.net/1515/bukkit/loader/LuckPerms-Bukkit-5.4.102.jar
 wget https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar
 wget https://github.com/NEZNAMY/TAB/releases/download/4.0.5/TAB.v4.0.5.jar
+wget https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar
+wget https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsXChat-2.20.1.jar
 wget https://github.com/dmulloy2/ProtocolLib/releases/latest/download/ProtocolLib.jar
 wget https://github.com/RainBowCreation/Core/releases/latest/download/RBCcore.jar
 cd ..
