0d4a9156892e0aa6cd39b96533c678bb6a67ea81
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-10-02 03:03:39 +0700

    sudo start test

--- node/start.sh
+++ node/start.sh
@@ -4,4 +4,4 @@
 NODE_NAME=$(cat /proc/sys/kernel/random/uuid)
 
 # start multipaper node
-java ${JAVA_OPTS} -jar Magma-1.19.3-44.1.23-6e6ce905-server.jar --nogui
+sudo java ${JAVA_OPTS} -jar Magma-1.19.3-44.1.23-6e6ce905-server.jar --nogui
