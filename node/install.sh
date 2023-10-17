56564e2190f800b03054f67e2b1be0fe3150cc96
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-10-17 13:26:42 +0700

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -4,8 +4,8 @@ cd plugins
 wget https://github.com/ViaVersion/ViaVersion/releases/download/4.8.0/ViaVersion-4.8.0.jar
 wget https://github.com/ViaVersion/ViaBackwards/releases/download/4.8.0/ViaBackwards-4.8.0.jar
 wget https://download.luckperms.net/1515/bukkit/loader/LuckPerms-Bukkit-5.4.102.jar
-wget https://github.com/MilkBowl/Vault/releases/download/1.7.3/Vault.jar
-wget https://github.com/NEZNAMY/TAB/releases/download/4.0.5/TAB.v4.0.5.jar
+wget https://dev.bukkit.org/projects/vault/files/894359/download
+wget https://github.com/NEZNAMY/TAB/releases/download/4.0.7/TAB.v4.0.7.jar
 wget https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar
 wget https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsXChat-2.20.1.jar
 wget https://github.com/dmulloy2/ProtocolLib/releases/latest/download/ProtocolLib.jar
