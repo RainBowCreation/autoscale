e5bf94f062212c405aec40ef6d6ab01843bf8c2f
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-10-17 17:00:33 +0700

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -3,8 +3,8 @@ git clone -b "1.12.2" https://github.com/RainBowCreation/mods.git
 git clone -b "1.12.2" https://github.com/RainBowCreation/world.git
 wget https://git.magmafoundation.org/api/v4/projects/7/packages/maven/org/magmafoundation/Magma/1.12.2-7d6be591/Magma-1.12.2-7d6be591.jar
 cd plugins
-wget https://github.com/ViaVersion/ViaVersion/releases/download/4.8.0/ViaVersion-4.8.0.jar
-wget https://github.com/ViaVersion/ViaBackwards/releases/download/4.8.0/ViaBackwards-4.8.0.jar
+# wget https://github.com/ViaVersion/ViaVersion/releases/download/4.8.0/ViaVersion-4.8.0.jar
+# wget https://github.com/ViaVersion/ViaBackwards/releases/download/4.8.0/ViaBackwards-4.8.0.jar
 wget https://download.luckperms.net/1515/bukkit/loader/LuckPerms-Bukkit-5.4.102.jar
 wget https://dev.bukkit.org/projects/vault/files/894359/download
 wget https://github.com/NEZNAMY/TAB/releases/download/4.0.7/TAB.v4.0.7.jar
