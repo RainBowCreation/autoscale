99c73b01cf776c9575ffb719789582ca50f23456
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-10-17 14:11:31 +0700

    Update install-debian.sh

--- lib/install-debian.sh
+++ lib/install-debian.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt install software-properties-common -y
 sudo apt install apt-transport-https lsb-release ca-certificates curl dirmngr gnupg
-sudo sh -c 'echo "deb http://deb.debian.org/debian $(lsb_release -sc) main contrib non-free" >> /etc/apt/sources.list.d/debian.list'
+sudo apt-add-repository -y 'deb http://security.debian.org/debian-security stretch/updates main'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 sudo apt update
-sudo apt install openjdk-17-jdk -y
+sudo apt install default-jdk -y
 sudo apt install git tmux cifs-utils -y
