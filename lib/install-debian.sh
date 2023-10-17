d1e3d7e53881a283af090837b2fe87f3e11fa8cc
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-10-17 14:59:01 +0700

    Update install-debian.sh

--- lib/install-debian.sh
+++ lib/install-debian.sh
@@ -7,5 +7,5 @@ sudo apt-add-repository -y 'deb http://security.debian.org/debian-security stret
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
 sudo apt update
 sudo apt install git tmux cifs-utils -y
-cd jdk8u[something]
-cd bin
+sudo tar -xf OpenJDK8U-jdk_x64_linux_hotspot_8u382b05.tar.gz
+export PATH=$PATH:/share/RainBowCreation/autoscale/lib/jdk8u382-b05/bin
