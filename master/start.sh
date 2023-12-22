2f3edf69eba788289dec52dbdb34b0261a5b712f
VocanicZ <ampnattahapat@gmail.com>
2023-12-22 14:55:59 +0700

    move master node

--- master/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xmx2G -jar master.jar 35353 $PORT
--- master/synced-server-files/plugins/install.sh
+++
@@ -1,4 +0,0 @@
-#bin/bash
-echo "installing plugins"
-wget https://www.spigotmc.org/resources/viaversion.19254/download?version=521999
-echo "done"
