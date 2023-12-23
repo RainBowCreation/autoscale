87e2a45eaff307a8605eb0bcfbb1e398f24da063
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-23 15:18:15 +0700

    Update install.sh

--- master/synced-server-files/plugins/install.sh
+++ master/synced-server-files/plugins/install.sh
@@ -1,4 +1,5 @@
 #bin/bash
 echo "installing plugins"
 wget https://github.com/ViaVersion/ViaVersion/releases/download/4.9.2/ViaVersion-4.9.2.jar
+wget "https://www.spigotmc.org/resources/perversionresources.74360/download?version=419853"
 echo "done"
