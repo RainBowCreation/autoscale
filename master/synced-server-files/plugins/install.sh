d5c316e80b8baf8bbc94b6010b8777e4c410adf5
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-25 17:45:22 +0700

    Update install.sh

--- master/synced-server-files/plugins/install.sh
+++ master/synced-server-files/plugins/install.sh
@@ -2,6 +2,4 @@
 echo "installing plugins"
 wget "https://github.com/ViaVersion/ViaVersion/releases/download/4.9.2/ViaVersion-4.9.2.jar"
 wget "https://github.com/ViaVersion/ViaBackwards/releases/download/4.9.1/ViaBackwards-4.9.1.jar"
-wget "https://www.spigotmc.org/resources/perversionresources.74360/download?version=419853"
-wget "https://www.spigotmc.org/resources/easycommandblocker-spigot-waterfall-velocity-1-8-1-20.101752/download?version=499638"
 echo "done"
