c9b1f2444483c403fc9e27245c802a3e17527aed
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-25 17:55:58 +0700

    Update install.sh

--- master/install.sh
+++ master/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/14/downloads/multipaper-master-2.11.0-all.jar
+git clone "https://github.com/RainBowCreation/world.git"
 mv multipaper-master-2.11.0-all.jar master.jar
