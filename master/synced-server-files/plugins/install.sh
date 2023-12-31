c0a7695f21777059db7534ec0c10ecd87b107a44
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-31 19:09:22 +0700

    Update install.sh

--- master/synced-server-files/plugins/install.sh
+++ master/synced-server-files/plugins/install.sh
@@ -2,4 +2,5 @@
 echo "installing plugins"
 wget "https://github.com/ViaVersion/ViaVersion/releases/download/4.9.2/ViaVersion-4.9.2.jar"
 wget "https://github.com/ViaVersion/ViaBackwards/releases/download/4.9.1/ViaBackwards-4.9.1.jar"
+wget "https://github.com/lucko/BungeeGuard/releases/download/v1.3.3/BungeeGuard.jar"
 echo "done"
