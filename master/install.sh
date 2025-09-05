2da406d215ee01a2cbab5c4a694e25d7e9cef178
NathanielSong <ampnattahapat@gmail.com>
2025-09-05 21:14:25 +0700

    fix amzn wget

--- master/install.sh
+++ master/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "downloading multipaper-master.."
-wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/60/downloads/multipaper-master-2.12.3-all.jar master.jar
+wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/60/downloads/multipaper-master-2.12.3-all.jar
+mv multipaper-master-2.12.3-all.jar master.jar
 echo "downloading plugins.."
 cd synced-serverfiles
 wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.2/PAPER/ViaVersion-5.4.2.jar
--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "downloading multipaper1.20.1.."
-wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/60/downloads/multipaper-1.20.1-60.jar node.jar
+wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/60/downloads/multipaper-1.20.1-60.jar
+mv multipaper-1.20.1-60.jar node.jar
 echo "done"
