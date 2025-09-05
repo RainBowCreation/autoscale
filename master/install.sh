5c163ab2ec0f0233f9f283c40347ed0dbf7dfcd5
NathanielSong <ampnattahapat@gmail.com>
2025-09-05 21:18:49 +0700

    fix plugin dir

--- master/install.sh
+++ master/install.sh
@@ -3,7 +3,7 @@ echo "downloading multipaper-master.."
 wget https://api.multipaper.io/v2/projects/multipaper/versions/1.20.1/builds/60/downloads/multipaper-master-2.12.3-all.jar
 mv multipaper-master-2.12.3-all.jar master.jar
 echo "downloading plugins.."
-cd synced-serverfiles
+cd synced-server-files
 wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.2/PAPER/ViaVersion-5.4.2.jar
 cd ..
 echo "done"
