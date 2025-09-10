d04360aca07f7d670399ca29ac9ba2c839350b4b
NathanielSong <ampnattahapat@gmail.com>
2025-09-10 18:24:56 +0700

    update nodeuitls and shorten server name

--- master/install.sh
+++ master/install.sh
@@ -6,11 +6,13 @@ echo "downloading plugins.."
 cd synced-server-files
 mkdir plugins
 cd plugins
-wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/RainBowCreation-bootstrap.jar
 wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/ViaVersion.jar
-wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/Vault.jar
-wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/CloudWatch.jar
+wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/PlaceholderAPI.jar
 wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/ProtocalLib.jar
+wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/Vault.jar
 wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/Regionerator.jar
+wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/multipaperplaceholders.jar
+wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/CloudWatch.jar
+wget https://repo.rainbowcreation.net/files/jar/autoscale_utils/RainBowCreation-bootstrap.jar
 cd ../..
 echo "done"
